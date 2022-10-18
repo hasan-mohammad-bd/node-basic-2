c050c96780aed647884e1b2e66e9c222fe2d8331
Hasan Mohammad <hasan.md.east@gmail.com>
2022-10-18 11:22:17 +0600

    adding some info

--- 2-10-Event.js
+++ 2-10-Event.js
@@ -1,10 +1,15 @@
 const events = require('events')
-const eventEmitter = new events.eventEmitter()
+const eventEmitter = new events.EventEmitter()
 
 
 const chitharDibo=()=>{
     console.log("oi hala koi asos");
 }
 
+//assign the handler into an event. 
+
 eventEmitter.on('screem', chitharDibo).chitharDibo
 
+//firing the event
+
+eventEmitter.emit('screem')
---
+++ 2-10-buffer.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+
+const readstream = fs.createReadStream('./data.txt')
+
+readstream.on('data', (chunk)=> {
+    console.log(('....'));
+    console.log(chunk);
+})
